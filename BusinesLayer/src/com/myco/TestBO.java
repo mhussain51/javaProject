b4d547d6437b6db8dfec68c5d8040e736bba7a9d
mutahir <mutahirh@gmail.com>
2021-08-02 18:04:11 -0400

    test project

---
+++ BusinesLayer/src/com/myco/TestBO.java
@@ -0,0 +1,10 @@
+package com.myco;
+
+public class TestBO {
+	private static final String NAME="Junk";
+	
+	public String getName() {		
+		return NAME;
+	}
+
+}
---
+++ MyGitTest/src/EntryPoint.java
@@ -0,0 +1,14 @@
+import com.myco.TestBO;
+
+public class EntryPoint {
+
+	public static void main(String[] args) {
+		
+		TestBO tbo = new TestBO();
+		
+		System.out.print(tbo.getName());
+		
+
+	}
+
+}
